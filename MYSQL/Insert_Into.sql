96c66fe36fef0d74b6471a21c1128bfb17b34ed8
AdamXu <xsz1993@live.com>
2021-08-30 14:45:15 +0800

    SQL_20210830-1445

---
+++ MYSQL/Insert_Into.sql
@@ -0,0 +1,12 @@
+start transaction ;
+
+insert into shop.shohin values ('0001', 'T恤','衣物',1000,500,'2009-09-20');
+insert into shop.shohin values ('0002', '打孔機','辦公用品',500,320,'2009-09-11');
+insert into shop.shohin values ('0003', '襯衫','衣物',4000,2800,null);
+insert into shop.shohin values ('0004', '菜刀','厨房用品',3000,2800,'2009-09-20');
+insert into shop.shohin values ('0005', '壓力鍋','厨房用品',6800,5000,'2009-01-15');
+insert into shop.shohin values ('0006', '叉子','厨房用品',500,null,'2009-09-20');
+insert into shop.shohin values ('0007', '刨絲器','厨房用品',880,790,'2008-04-28');
+insert into shop.shohin values ('0008', '鋼珠筆','辦公用品',400,null,'2009-11-11');
+
+commit ;
