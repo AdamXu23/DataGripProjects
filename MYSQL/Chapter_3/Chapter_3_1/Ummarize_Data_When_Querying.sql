d264f6f7425305ab974a5ed7a10be2bd53549a00
AdamXu <xsz1993@live.com>
2021-09-02 19:01:34 +0800

    SQL_20210902-1701

--- MYSQL/Chapter_3/Chapter_3_1/Ummarize_Data_When_Querying.sql
+++ MYSQL/Chapter_3/Chapter_3_1/Ummarize_Data_When_Querying.sql
@@ -10,3 +10,9 @@ from shop.shohin;
 -- MAX MIN --
 select max(reg_Date),min(reg_Date)
 from shop.shohin;
+-- 先排除重複值在計算 --
+select count(distinct Shohin_catalg)
+from shop.shohin;
+-- 先計算在排除重複值 --
+select distinct count(Shohin_catalg)
+from shop.shohin;
