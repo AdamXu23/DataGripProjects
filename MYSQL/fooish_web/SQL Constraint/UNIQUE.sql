5496a374b176e3ea897b4bd7dd3c24c644d2afc3
AdamXu23 <xsz1993@live.com>
2021-09-13 20:07:07 +0800

    SQL_20210913_1906

--- MYSQL/fooish_web/SQL Constraint/UNIQUE.sql
+++ MYSQL/fooish_web/SQL Constraint/UNIQUE.sql
@@ -31,3 +31,35 @@ CREATE TABLE customer (
   Phone VARCHAR(20),
   CONSTRAINT u_Customer_Id UNIQUE (C_Id, Name)
 );
+INSERT INTO customer (C_Id, Name, Address, Phone)
+VALUES (3, '李三', 'ZZ路300號', '07-12345678');
+INSERT INTO customer (C_Id, Name, Address, Phone)
+VALUES (4, '李三', 'ZZ路300號', '07-12345678');
+INSERT INTO customer (C_Id, Name, Address, Phone)
+VALUES (4, '李四', 'ZZ路300號', '07-12345678');
+INSERT INTO customer (C_Id, Name, Address, Phone)
+VALUES (4, '李四', 'ZZ路300號', '07-12345679');
+
+-- 僅刪除資料表內容，但保留結構 (TRUNCATE TABLE) --
+TRUNCATE TABLE customer;
+-- 更改資料表限制 ALTER TABLE...
+ALTER TABLE customer ADD UNIQUE (C_Id);
+
+-- 刪除資料表 (DROP TABLE) --
+DROP TABLE customer;
+
+-- 替唯一鍵命名與多欄位的唯一限制
+CREATE TABLE customer (
+  C_Id INT NOT NULL,
+  Name VARCHAR(50) NOT NULL,
+  Address VARCHAR(255),
+  Phone VARCHAR(20)
+);
+-- 替唯一鍵命名與多欄位的唯一限制：
+ALTER TABLE customer
+ADD CONSTRAINT u_Customer_Id UNIQUE (C_Id, Name);
+
+-- 移除資料表限制 ALTER TABLE...
+
+ALTER TABLE customer DROP INDEX u_Customer_Id;
+ALTER TABLE customer DROP INDEX C_Id;
