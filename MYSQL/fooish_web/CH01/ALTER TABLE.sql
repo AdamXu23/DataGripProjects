736c3498859bf953d0e727f36878ce513f1be20d
AdamXu23 <xsz1993@live.com>
2021-09-13 18:52:39 +0800

    SQL_20210913_1852

--- MYSQL/fooish_web/CH01/ALTER TABLE.sql
+++ MYSQL/fooish_web/CH01/ALTER TABLE.sql
@@ -2,5 +2,6 @@
 ALTER TABLE customers ADD Discount VARCHAR(10);
 -- 更改欄位資料型別 (ALTER COLUMN TYPE) --
 -- MYSQL --
-ALTER TABLE customers
-    MODIFY COLUMN discount DECIMAL(18, 2);
+ALTER TABLE customers MODIFY COLUMN discount DECIMAL(18, 2);
+-- 刪除欄位 (DROP COLUMN) --
+ALTER TABLE customers DROP COLUMN Discount;
