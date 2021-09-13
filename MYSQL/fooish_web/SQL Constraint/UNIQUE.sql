17e85be4641d8cfde0ba817b11d14109bd2fb110
AdamXu23 <xsz1993@live.com>
2021-09-13 19:09:13 +0800

    SQL_20210913_1909

--- MYSQL/fooish_web/SQL Constraint/UNIQUE.sql
+++ MYSQL/fooish_web/SQL Constraint/UNIQUE.sql
@@ -20,3 +20,14 @@ CREATE TABLE customer (
 
 -- 刪除資料表 (DROP TABLE) --
 DROP TABLE customer;
+
+-- 替唯一鍵命名與多欄位的唯一限制
+-- 我們限制 C_Id 及 Name 這兩個欄位共同唯一，CONSTRAINT 後面接著的即是此唯一鍵的名稱。
+
+CREATE TABLE customer (
+  C_Id INT NOT NULL,
+  Name VARCHAR(50) NOT NULL,
+  Address VARCHAR(255),
+  Phone VARCHAR(20),
+  CONSTRAINT u_Customer_Id UNIQUE (C_Id, Name)
+);
