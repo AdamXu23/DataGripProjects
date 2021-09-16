1b7c14e2e7f02ccc8dbf36dfb818e75321a3c33e
AdamXu23 <xsz1993@live.com>
2021-09-16 11:20:30 +0800

    SQL-20210916-1120

--- MSSQLLocal/DATABASE/ch01 CREATE DATABASE .sql
+++ MSSQLLocal/DATABASE/ch01 CREATE DATABASE .sql
@@ -1,14 +1,14 @@
 SELECT name, database_id, create_date
-FROM sys.databases
-where name = 'SOHC';
+FROM sys.databases;
 
-CREATE DATABASE SOHC;
+-- CREATE DATABASE SOHC;
 
-SELECT name, database_id, create_date
+/*SELECT name, database_id, create_date
 FROM sys.databases
 where name = 'SOHC';
 
 DROP DATABASE SOHC;
+*/
 
 IF NOT EXISTS (select * from sys.databases where name = 'SOHC')
 CREATE DATABASE SOHC;
--- MSSQLLocal/Login_Table/ch02 INSERT.sql
+++ MSSQLLocal/Login_Table/ch02 INSERT.sql
@@ -1,2 +1,5 @@
 insert into Login_Table values ('Administrator','AVEX1234',0);
 insert into Login_Table values ('ADMIN','SOHC',1);
+
+-- select * from Login_Table where User_Name = 'ADMIN' collate Chinese_PRC_CS_AI;
+select * from Login_Table;
