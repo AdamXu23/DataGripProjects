2183989a14887fa9e6f1303cd1242b375a703a69
AdamXu <xsz1993@live.com>
2021-12-06 18:44:35 +0800

    20211206-0644

--- MSSQL/PROCEDURE/PROCEDURE.sql
+++ MSSQL/PROCEDURE/PROCEDURE.sql
@@ -1,1 +1,11 @@
-create procedure
+insert into Daytime (dt)
+values (getdate());
+
+create procedure INSERT_Daytime
+as
+begin
+insert into Daytime (dt)
+values (getdate());
+end ;
+
+exec INSERT_Daytime;
---
+++ MSSQL/SQL Server Agent/sp_add_schedule .sql
@@ -0,0 +1,8 @@
+exec sp_add_
+    @schedule_name = N'NightlyJobs' ,
+    @freq_type = 4,
+    @freq_interval = 1,
+    @active_start_time = 010000 ;
+GO
+
+
