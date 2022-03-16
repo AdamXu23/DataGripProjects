c47811d520a9001d5d8ea3e44034f7438f06ffe7
AdamXu <xsz1993@live.com>
2022-03-16 20:47:59 +0800

    20220316-08947

--- MSSQL/PTLP/RH_TABLE.sql
+++ MSSQL/PTLP/RH_TABLE.sql
@@ -5,7 +5,7 @@ create table RH_Table
     RH_ID                   uniqueidentifier    DEFAULT (newid())   not null ,
     RH_Serial_Number        varchar(20)                             not null ,
     RH_Firmware             varchar(20)                             not null ,
-    RH_mode                 varchar(20)                             not null ,
+    RH_model                varchar(20)                             not null ,
     DT                      datetime2           default (getdate()) not null ,
     CONSTRAINT PK_RH_ID_RH_Table PRIMARY KEY (RH_ID)
 );
